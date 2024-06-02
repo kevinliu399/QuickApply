5636235b517ae5cc3e9a1689979bf93f3521dfad
Kevin Liu <kevinliu399@gmail.com>
2024-06-01 20:35:30 -0400

    colors

--- src/modals/login.tsx
+++ src/modals/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CircleX } from 'lucide-react'
+import { CircleX } from 'lucide-react';
 import { Input } from '@mui/material'; 
 import { Button } from '@mui/material';
 
@@ -16,21 +16,24 @@ const LoginModal: React.FC<LoginModalProps> = ({ onClick, password, username, em
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 flex items-center pl-54 justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded-lg shadow-lg w-1/2">
-                <div className="flex justify-between items-center">
-                    <h1 className="text-2xl font-bold mb-4">Register</h1>
-                    <CircleX size={20} onClick={onClick}/>
+        <div className="fixed inset-0 flex bg-black bg-opacity-50">
+            <div className="absolute top-0 bottom-0 left-64 right-0 flex items-center justify-center">
+                <div className="bg-white p-4 rounded-lg shadow-lg w-1/2 items-center justify-center">
+                    <div className="flex justify-between items-center py-6">
+                        <h1 className="text-2xl font-bold">Log In</h1>
+                        <CircleX size={20} onClick={onClick} className="cursor-pointer hover:text-main-green hover:duration-200" />
+                    </div>
+                    <div className="flex flex-col space-y-5">
+                        <Input placeholder="Username" value={username} onChange={(e) => {}} className="mb-2 p-2 border rounded"/>
+                        <Input placeholder="Email" value={email} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
+                        <Input placeholder="Password" value={password} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
+                    </div>
+                    <div className="py-2 items-center w-full justify-center">
+                        <Button onClick={onClick} className="bg-blue-800 text-white p-2 rounded">Sign In</Button>
+                    </div>
+                    <p className="mt-4">Don't have an account? <a className="text-blue-800 cursor-pointer" onClick={onSignUpClick}>Register</a></p>    
                 </div>
-                <div className="flex flex-col space-y-5">
-                    <Input placeholder="Username" value={username} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
-                    <Input placeholder="Email" value={email} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
-                    <Input placeholder="Password" value={password} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
-                    <Button onClick={onClick} className="bg-blue-800 text-white p-2 rounded">Register</Button>
-                </div>
-                <p className="mt-4">Already have an account? <a className="text-blue-800 cursor-pointer" onClick={onSignUpClick}>Sign In</a></p>    
             </div>
-            
         </div>
     );
 }
--- src/modals/register.tsx
+++ src/modals/register.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { CircleX } from 'lucide-react'
+import { CircleX } from 'lucide-react';
 import { Input } from '@mui/material'; 
 import { Button } from '@mui/material';
 
-type LoginModalProps = {
+type RegisterModalProps = {
     onClick?: () => void;
     onSignInClick?: () => void;
     password: string;
@@ -12,28 +12,30 @@ type LoginModalProps = {
     isOpen?: boolean;
 }
 
-const LoginModal: React.FC<LoginModalProps> = ({ onClick, password, username, email, isOpen, onSignInClick }) => {
+const RegisterModal: React.FC<RegisterModalProps> = ({ onClick, password, username, email, isOpen, onSignInClick }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded-lg shadow-lg">
-                <div className="flex-row w-full">
-                    <div className="">
-                        <h1 className="flex-1 text-2xl font-bold mb-4">Register</h1>
-                        <CircleX size={20}/>
+        <div className="fixed inset-0 flex bg-black bg-opacity-50">
+            <div className="absolute top-0 bottom-0 left-64 right-0 flex items-center justify-center">
+                <div className="bg-white p-6 rounded-lg shadow-lg w-1/2">
+                    <div className="flex justify-between items-center">
+                        <h1 className="text-2xl font-bold mb-4">Register</h1>
+                        <CircleX size={20} onClick={onClick} className="cursor-pointer"/>
                     </div>
+                    <div className="flex flex-col space-y-5">
+                        <Input placeholder="Username" value={username} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
+                        <Input placeholder="Email" value={email} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
+                        <Input placeholder="Password" value={password} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
+                    </div>
+                    <div className="py-2 items-center w-full justify-center">
+                        <Button onClick={onClick} className="bg-blue-800 text-white p-2 rounded">Register</Button>
+                    </div>
+                    <p className="mt-4">Already have an account? <a className="text-blue-800 cursor-pointer" onClick={onSignInClick}>Sign In</a></p>    
                 </div>
-                <div className="flex-col space-x-5">
-                    <Input placeholder="Username" value={username} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
-                    <Input placeholder="Email" value={email} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
-                    <Input placeholder="Password" value={password} onChange={(e) => {}} className="mb-2 p-2 border rounded" />
-                    <Button onClick={onClick} className="bg-blue-800 text-white p-2 rounded">Register</Button>
-                </div>
-                <p className="mt-4">Already have an account? <a className="text-blue-800" onClick={onSignInClick}>Sign In</a></p>
             </div>
         </div>
     );
 }
 
-export default LoginModal;
+export default RegisterModal;
